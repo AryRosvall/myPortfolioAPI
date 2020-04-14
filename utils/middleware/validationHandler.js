a4bb818379e129b3d43ff5d2d1cba8c6db33a849
AryRosvall <ary.rosvall@gmail.com>
2020-04-13 20:06:08 -0500

    [ADD] validationHandler middleware and joi & boom to validate the incoming data

---
+++ utils/middleware/validationHandler.js
@@ -0,0 +1,16 @@
+const joi = require('@hapi/joi');
+const boom = require('@hapi/boom');
+
+function validate(data, schema) {
+  const {error} = joi.object(schema).validate(data);
+  return error;
+}
+
+function validationHandler(schema, check = 'body') {
+  return function (req, res, next) {
+    const error = validate(req[check], schema);
+    error ? next(boom.badRequest(error)) : next();
+  }
+}
+
+module.exports = validationHandler;
