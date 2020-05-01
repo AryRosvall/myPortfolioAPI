145c0b462d0070b5e62efec2d5191128091ae7d8
AryRosvall <ary.rosvall@gmail.com>
2020-04-30 20:52:47 -0500

    [ADD] Entry point server.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const config = require('../config');
 const profile = require('./components/profile/network');
 const experience = require('./components/experiences/network');
 const language = require('./components/languages/network');
@@ -7,28 +5,16 @@ const academic = require('./components/academics/network');
 const skills = require('./components/skills/network');
 const projects = require('./components/projects/network');
 const email = require('./components/mailing/network');
-const cors = require('cors');
 
-//Initialize app
-const app = express();
 
-app.use(express.json());
+module.exports = (app) => {
 
-//Habilitate CORS only to the frontend URL
-app.use(cors({ origin: true, credentials: true }));
+  app.use('/api/profile', profile);
+  app.use('/api/experience', experience);
+  app.use('/api/language', language);
+  app.use('/api/academic', academic);
+  app.use('/api/skill', skills);
+  app.use('/api/project', projects);
+  app.use('/api/email', email);
 
-app.get('/', function (req, res) {
-  res.redirect('/api/profile');
-});
-
-app.use('/api/profile', profile);
-app.use('/api/experience', experience);
-app.use('/api/language', language);
-app.use('/api/academic', academic);
-app.use('/api/skill', skills);
-app.use('/api/project', projects);
-app.use('/api/email', email);
-
-app.listen(config.api.port, () => {
-  console.log('Listening in port ', config.api.port);
-});
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const config = require('./config');
+const Routes = require('./api')
+
+const cors = require('cors');
+
+//Initialize app
+const app = express();
+
+app.use(express.json());
+
+//Habilitate CORS only to the frontend URL
+app.use(cors({ origin: true, credentials: true }));
+
+Routes(app);
+
+app.get('/', (req, res, next) => {
+  try {
+    res.status(200)
+      .send({
+        "api": "aryrosvall.com",
+        "version": "1.0.0"
+      });
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(config.api.port, () => {
+  console.log('Listening in port ', config.api.port);
+});
